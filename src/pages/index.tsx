123d3a417d4561d3a593ba9ebbf86314677109c5
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-12-03 10:16:07 -0300

    :art: Style(Home): Button Slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,35 +47,50 @@ export default function Home() {
           className="mySwiper"
         >
           <SwiperSlide>
-            <Slide backgroundImage="/images/slide01.jpg">
+            <Slide backgroundImage="/images/slide01A.jpg">
               <h1 data-aos="fade-right">Projetos Elétricos</h1>
               <p data-aos="fade-left">
                 Residencial, comercial, industrial, loteamento, posto de
                 transformação rural e sob medida para sua empresa.
               </p>
-              <ButtonOrcamento data-aos="fade-in" data-aos-duration="2000">
-                Faça um orçamento
+              <ButtonOrcamento
+                href="https://api.whatsapp.com/send?phone=5517996353232"
+                target="_blank"
+                data-aos="fade-in"
+                data-aos-duration="500"
+              >
+                Entrar em contato
               </ButtonOrcamento>
             </Slide>
           </SwiperSlide>
           <SwiperSlide>
-            <Slide backgroundImage="/images/slide02.jpg">
+            <Slide backgroundImage="/images/slide02B.jpg">
               <h1>ART</h1>
               <p>
                 ART para ligação de padrão à pedido da concessionária de
                 energia.
               </p>
-              <ButtonOrcamento>Faça um orçamento</ButtonOrcamento>
+              <ButtonOrcamento
+                href="https://api.whatsapp.com/send?phone=5517996353232"
+                target="_blank"
+              >
+                Entrar em contato
+              </ButtonOrcamento>
             </Slide>
           </SwiperSlide>
           <SwiperSlide>
-            <Slide backgroundImage="/images/slide03.jpg">
+            <Slide backgroundImage="/images/slide03C.jpg">
               <h1>Projeto de energia solar</h1>
               <p>
                 Residencial, comercial, industrial, loteamento, posto de
                 transformação rural e sob medida para sua empresa.
               </p>
-              <ButtonOrcamento>Faça um orçamento</ButtonOrcamento>
+              <ButtonOrcamento
+                href="https://api.whatsapp.com/send?phone=5517996353232"
+                target="_blank"
+              >
+                Entrar em contato
+              </ButtonOrcamento>
             </Slide>
           </SwiperSlide>
         </Swiper>
