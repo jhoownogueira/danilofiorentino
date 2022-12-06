817be4df9249a350e543b05af0dd4b81c6ff4865
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-12-06 15:45:02 -0300

    :art: (Style) SobreMim: Animate Div

--- src/pages/sobre-mim.tsx
+++ src/pages/sobre-mim.tsx
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import { Banner, FormEng, SobreMimContainer } from "../styles/pages/SobreMim";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 export default function Sobremim() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,7 +20,7 @@ export default function Sobremim() {
       </Head>
       <SobreMimContainer>
         <div className="grid">
-          <div className="left">
+          <div className="left" data-aos="fade-right" data-aos-duration="1000">
             <h1>Danilo Fiorentino</h1>
             <span>
               Engenheiro Eletricista & Engenheiro de Segurança do Trabalho
@@ -24,26 +35,36 @@ export default function Sobremim() {
             </p>
           </div>
           <div className="right">
-            <img src="/images/daniloSobreMim.png" className="engenheiro" />
-            <img src="/images/circlesSobreMim.png" className="circles" />
+            <img
+              src="/images/daniloSobreMim.png"
+              className="engenheiro"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+            />
+            <img
+              src="/images/circlesSobreMim.png"
+              className="circles"
+              data-aos="fade-in"
+              data-aos-duration="1000"
+            />
           </div>
         </div>
       </SobreMimContainer>
       <Banner>
         <div className="grid">
-          <div className="info">
+          <div className="info" data-aos="fade-up" data-aos-duration="1500">
             <h4>10+</h4>
             <span>Projetos concluídos</span>
           </div>
-          <div className="info">
+          <div className="info" data-aos="fade-down" data-aos-duration="1500">
             <h4>9+</h4>
             <span>Anos de experiência</span>
           </div>
-          <div className="info">
+          <div className="info" data-aos="fade-up" data-aos-duration="1500">
             <h4>50+</h4>
             <span>Projetos clientes</span>
           </div>
-          <FormEng>
+          <FormEng data-aos="fade-up" data-aos-duration="1000">
             <h4>Tire suas dúvidas comigo</h4>
             <div className="field">
               <label htmlFor="nome">Nome</label>
