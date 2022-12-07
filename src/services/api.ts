813bf34bfeb5f9f634ba1db832f92844d0033221
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-12-07 17:29:30 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://sendgrid-danilofiorentino.herokuapp.com/",
+    baseURL: "https://api-danilo.onrender.com/",
   });
